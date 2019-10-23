f96c3b354cc3c36bf909313490538964189ae49e
asrianijainudin <55940765+asrianijainudin@users.noreply.github.com>
2019-10-23 22:23:09 +0900

    tugas3

---
+++ pgame-16001/code-tugas3.py
@@ -0,0 +1,12 @@
+# Ini Tugas 3. Praktik Git untuk MK Pemrograman Game
+# juga untuk latihan git push dan pull from terminal
+
+# Latihan Buat Variabel : 
+nama = "Nama 		: ASRIANI JAINUDIN" 
+print (nama)
+npm = "NPM 		: 121055520116001" 
+print (npm)
+semester = "Semester 	: VII (Tujuh)" 
+print (semester)
+matakuliah = "Mata Kuliah 	: Pemograman Game" 
+print (matakuliah)
