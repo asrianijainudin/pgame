66fb4b114a19f8760b60543bb425688785389659
asrianijainudin <55940765+asrianijainudin@users.noreply.github.com>
2019-10-23 22:20:19 +0900

    tugas2

---
+++ pgame-16001/code-tugas2.py
@@ -0,0 +1,8 @@
+# Ini Tugas 2. Praktik Git untuk MK Pemrograman Game
+
+print ("Hello, my friends") #Halo, teman-teman
+print ("my name is Wuyun Hamid") #nama saya Wuyun Hamid
+print ("How are you") #apakabar
+#baris ini tidak akan dieksekusi
+print ("computing friends...") #baris ini dieksekusi
+#Baris ini juga merupakan Baris Komentar 
