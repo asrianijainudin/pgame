84ba981fda073f472cb4d01f92480316756fbb1c
asrianijainudin <55940765+asrianijainudin@users.noreply.github.com>
2019-11-08 01:41:18 +0900

    Modul_Pgame_Operator_Prak_3

---
+++ pgame-16001/code-tugas8.py
@@ -0,0 +1,18 @@
+#Operator Penugasan
+
+a = 10 # Buat variabel a dan isi dengan nilai 10
+print (a)
+a += 10 # Tambahkan nilai 10 ke variabel a
+print (a)
+a -= 19
+print (a)
+a *= 15
+print (a)
+a /= 25
+print (a)
+a %= 30
+print (a)
+a **= 25
+print (a)
+a //= 60
+print (a)
