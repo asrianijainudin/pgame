59d74e0d5bf6ba9ab36a62a9a82952bf385e203b
asrianijainudin <55940765+asrianijainudin@users.noreply.github.com>
2019-11-08 02:07:07 +0900

    Modul_Pgame_Operator_Prak_3

---
+++ pgame-16001/code-tugas11.py
@@ -0,0 +1,13 @@
+#Operator Identitas
+
+a,b = 33,44
+a & b # Melakukan operasi and pada biner a dan b
+a | b # Melakukan operasi or pada biner a dan b
+a,b = 33,44
+a ^ b
+a,b = 33,33
+~a
+a,b = 33,44
+a << 2
+a,b = 33,44
+a >> 2
